0919cfd30c30e43674daba85374d019f7b745069
CU-MUSIC <h4yddd@gmail.com>
2026-01-03 16:40:31 +0900

    Add files via upload

---
+++ album-select.js
@@ -0,0 +1,18 @@
+/**
+ * C/U MUSIC - 专辑数据库
+ * 在此处添加、删除或修改作品记录
+ */
+const albums = [
+	{ link: "https://music.163.com/album?id=263242113", image: "https://p1.music.126.net/QpNRZsfMP5J4tOlziA8Pnw==/109951170504642760.jpg", year: "2025", artist: "Rapeter", title: "IMGOOD别担心我", credits:["cookdao"] },
+	{ link: "https://music.163.com/album?id=350777340", image: "https://p1.music.126.net/_-rFuALruNFFlRfoEslp7A==/109951172303441948.jpg", year: "2025", artist: "Føxco", title: "Fear Of Nothing", credits:["cookdao"] },
+	{ link: "https://music.163.com/album?id=349892857", image: "https://p1.music.126.net/Z0XW7Vb1WHMnn0Z0aZ3tVw==/109951172263038796.jpg", year: "2025", artist: "安伊涵", title: "BAD DESIRE", credits:["cookdao","uin"] },
+	{ link: "https://music.163.com/album?id=273398421", image: "https://p2.music.126.net/EZUw2Idccw7OkinISOXpeQ==/109951171093017111.jpg", year: "2025", artist: "RioT婷", title: "小巴辣子", credits:["cookdao"] },
+	{ link: "https://music.163.com/album?id=285430095", image: "https://p2.music.126.net/fUvIMTUbos3A397_tb6Tzw==/109951172032384990.jpg", year: "2025", artist: "Uin", title: "首尔病(Take Me Out)", credits:["cookdao","uin"] },
+	{ link: "https://music.163.com/album?id=276925482", image: "https://p2.music.126.net/m0opMAaNB1eOGnd30EYFZg==/109951171387080337.jpg", year: "2025", artist: "焦娇JoJo", title: "陕北女子", credits:["cookdao"] },
+	{ link: "https://music.163.com/album?id=270573035", image: "https://p1.music.126.net/NrbdvLw7jhqKWzPYLUzEOw==/109951170927016553.jpg", year: "2025", artist: "YOUNGLEGO", title: "快人一步", credits:["cookdao"] },
+	{ link: "https://music.163.com/album?id=267552392", image: "https://p2.music.126.net/dLO3LfOGdQ-X3P6I02yrWA==/109951170673486612.jpg", year: "2025", artist: "Ele Jazz", title: "太空规则", credits:["cookdao"] },
+	{ link: "https://music.163.com/album?id=262350947", image: "https://p2.music.126.net/tg23znRitSin1BsH9cNRpw==/109951170470853706.jpg", year: "2025", artist: "Rapeter", title: "Shower", credits:["cookdao"] },
+	{ link: "https://music.163.com/album?id=258535006", image: "https://p1.music.126.net/K0FP9U0dC9uzP1MP4WrdIA==/109951170344988605.jpg", year: "2025", artist: "西冷", title: "果体，苹果，你", credits:["cookdao"] },
+	{ link: "https://music.163.com/album?id=247380991", image: "https://p1.music.126.net/oeWWJJjJ0c0erzqBoiVD3g==/109951169946775974.jpg", year: "2024", artist: "Cee", title: "侬CYPHER了伐", credits:["cookdao"] },
+	{ link: "https://music.163.com/album?id=189753745", image: "https://p1.music.126.net/rA734NyZbO-HTxi0L4CGcQ==/109951169433203470.jpg", year: "2024", artist: "Ele Jazz", title: "Palette Mixtape", credits:["cookdao"] }
+];
